6959d4675a7397668ab127a79bbefc623bb13541
max <max.r.johnson2021@gmail.com>
2024-11-15 06:42:49 -0600

    Add model for world to initialize a grid
    The grid should be initialized upon creating a world, simply creating a 2D array to represent rows and columns. The values of the cells will represent the state of that cell, which also influences the UI for the grid. Also include methods to get/set data in cells for later.

--- app/models/world.rb
+++ app/models/world.rb
@@ -1,2 +1,27 @@
 class World < ActiveRecord::Base
+  has_one :grid, dependent: :destroy
+  has_many :users
+
+  serialize :data, Array
+
+  def initialize_grid(rows = 6, cols = 6, default_value = "0")
+    self.data = Array.new(rows) { Array.new(cols, default_value) }
+    set(2,3, "1") #Temporarily fill a grid tile
+    save
+  end
+
+  def set(row, col, value)
+    self.data[row][col] = value
+    save
+  end
+
+  def get(row, col)
+    self.data[row][col] rescue nil
+  end
+
+  def display
+    self.data.each do |row|
+      puts row.join(" ")
+    end
+  end
 end
