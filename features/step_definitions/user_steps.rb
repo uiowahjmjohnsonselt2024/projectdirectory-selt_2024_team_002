19850eafc7ded0102d9856c27c0e6237cc5b6d5e
adervesh03 <adervesh03@gmail.com>
2024-11-15 14:16:20 -0600

    cucumber step definitions

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,19 +1,20 @@
 When(/^I am on the Create Account page$/) do
-  pending
+  visit new_user_path
 end
 
+When(/^I am on the Login page$/) do
+  visit users_login_path
+end
+
+# change to match the form fields
 And(/^I fill in "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
   pending
 end
 
 And(/^I press "([^"]*)"$/) do |arg|
-  pending
+  click_button arg
 end
 
 Then(/^I should see "([^"]*)"$/) do |arg|
-  pending
-end
-
-When(/^I am on the Login page$/) do
-  pending
+  expect(page).to have_content(arg)
 end
