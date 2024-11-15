3e90eb95773f57e1d5cf1cb99141638636e2641e
Alex Guo <aguo2@uiowa.edu>
2024-11-14 20:38:44 -0600

    fixed pass validation and added model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,34 +1,39 @@
 require 'bcrypt'
 
 class User < ActiveRecord::Base
-  validates :email, presence: { message: " is required." }, 'valid_email_2/email': true
-  validates :display_name, presence: { message: " is required." }, uniqueness: { message: " %{value} is taken" }
+  validates :email, presence: { message: "is required." }, 'valid_email_2/email': true
+  validates :display_name, presence: { message: "is required." }, uniqueness: { message: " %{value} is taken" }
   has_secure_password 
   validate :password_complexity
-  # verifies if login info is valid
-  
-  # updates the user's session token
   def update_session_token
     self.session_token = SecureRandom.hex(32)
     self.save
   end
 
   private
+  # one error a time to not show 20 errors at a ti
   def password_complexity
+    unless password_digest
+      return #handled by has secure password but causes rspec to fail
+    end
+    unless password_digest.length >= 12
+      errors.add(:password, "must be longer than 12 characters")
+      return
+    end
     unless password_digest.match?(/\d/)
-      errors.add(:password, " must include a digit")
+      errors.add(:password, "must include a digit")
       return
     end
     unless password_digest.match?(/[A-Z]/)
-      errors.add(:password, " must include one uppercase letter.")
+      errors.add(:password, "must include one uppercase letter")
       return
     end
     unless password_digest.match?(/[a-z]/)
-      errors.add(:password, " must include one lowercase letter.")
+      errors.add(:password, "must include one lowercase letter")
       return
     end
     unless password_digest.match?(/[\W_]/)
-      errors.add(:password, " must include one special character.")
+      errors.add(:password, "must include one special character")
       return
     end
   end
@@ -36,14 +41,12 @@ class User < ActiveRecord::Base
   # https://api.rubyonrails.org/v7.1/classes/ActiveModel/SecurePassword/ClassMethods.html
   # use authenticate for checking the password
   
-  # finds user by username
-  # returns User object if found, nil otherwise
+
   def self.find_user_by_display_name(display_name)
     User.where(["display_name = ?", display_name]).first
   end
 
-  # finds user by session token
-  # returns User object if found, nil otherwise
+
   def self.find_user_by_session_token(session_token)
     usr = User.where(["session_token = ?", session_token]).first
     usr
