b8977c3d192dda2870b8fcb4e8a3b661be278d4a
adervesh03 <adervesh03@gmail.com>
2024-11-28 17:13:49 -0600

    renaming

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -86,7 +86,7 @@ class UsersController < ApplicationController
       cookies[:session] = { value: @user.session_token, expires: 1.week.from_now }
       return redirect_to worlds_path
     end
-    flash[:alert] = 'Incorrect username and password'
+    flash[:alert] = 'Incorrect username or password'
     redirect_to users_login_path
   end
 
