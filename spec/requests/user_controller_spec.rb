8afc8aa217209ce17e18574ee208f3358e15ba47
Alex Guo <aguo2@uiowa.edu>
2024-11-29 22:48:03 -0500

    wip

--- spec/requests/user_controller_spec.rb
+++ spec/requests/user_controller_spec.rb
@@ -169,6 +169,46 @@ RSpec.describe 'Users', type: :request do
       expect(response).to redirect_to users_purchase_path
     end
   end
+
+  describe 'forgot password' do
+    it 'redirects to the correct location' do
+      post users_forgot_password_path
+      expect(response).to redirect_to users_login_path
+    end
+    # TODO: check email seam
+  end
+
+  describe 'plus user' do
+    it 'redirects successfully' do
+      user = instance_double(User)
+      allow(User).to receive(:find_user_by_session_token).and_return(user)
+      allow(user).to receive(:plus_user?).and_return(true)
+      post users_purchase_plus_user_path
+      expect(response).to redirect_to worlds_path
+    end
+  end
+
+  describe 'reject friend request' do
+    before do
+      usr = instance_double(User)
+      friend = instance_double(User)
+      allow(User).to receive(:find_by).and_return(friend)
+      allow(User).to receive(:find_user_by_session_token).and_return(usr)
+      allow(friend).to receive(:id).and_return(0)
+      allow(usr).to receive(:id).and_return(0)
+      allow(Friendship).to receive(:find_by).and_return(nil)
+    end
+
+    it 'sets the correct message' do
+      delete users_reject_request_path
+      expect(assigns(:message)).to eq('Friend request declined.')
+    end
+
+    it 'redirects correctly' do
+      delete users_reject_request_path
+      expect(response).to render_template('reject_request')
+    end
+  end
 end
 # rubocop:enable Metrics/BlockLength
 # rubocop:enable RSpec/ExampleLength
