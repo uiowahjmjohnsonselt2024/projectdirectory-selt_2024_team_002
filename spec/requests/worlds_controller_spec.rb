cd8ab8cda7ffdfe877eab82eee784652c14dd7bd
NikHerdt <herdtnikhil@gmail.com>
2024-12-04 12:03:47 -0600

    fixed lint

--- spec/requests/worlds_controller_spec.rb
+++ spec/requests/worlds_controller_spec.rb
@@ -3,7 +3,6 @@
 require 'spec_helper'
 require 'rails_helper'
 
-# rubocop:disable Metrics/BlockLength
 describe WorldsController do
   let(:cur_user) { instance_double(User, id: 1) }
   let(:world) { instance_double(World, id: 1, current_players: 1, max_player: 2) }
@@ -12,17 +11,14 @@ describe WorldsController do
     allow(User).to receive(:find_user_by_session_token).and_return(cur_user)
     allow(World).to receive(:find).and_return(world)
     allow(world).to receive(:update)
-    allow(UserWorld).to receive(:find_or_create_by).and_return(instance_double(UserWorld))
-    allow(UserWorld).to receive(:find_by).and_return(instance_double(UserWorld))
+    allow(UserWorld).to receive_messages(find_or_create_by: instance_double(UserWorld),
+                                         find_by: instance_double(UserWorld))
   end
 
   describe 'When a user is logged in' do
     before do
-      allow(cur_user).to receive(:display_name).and_return('')
-      allow(cur_user).to receive(:available_credits).and_return(0)
-      allow(cur_user).to receive(:plus_user?).and_return(false)
-      allow(Friendship).to receive(:friend_ids).and_return([])
-      allow(Friendship).to receive(:requested_friend_ids).and_return([])
+      allow(cur_user).to receive_messages(display_name: '', available_credits: 0, plus_user?: false)
+      allow(Friendship).to receive_messages(friend_ids: [], requested_friend_ids: [])
     end
 
     describe 'leave world' do
@@ -42,15 +38,13 @@ describe WorldsController do
 
     describe 'join world' do
       it 'increases the current_players count by 1' do
-        allow(world).to receive(:current_players).and_return(1)
-        allow(world).to receive(:max_player).and_return(2)
+        allow(world).to receive_messages(current_players: 1, max_player: 2)
         expect(world).to receive(:update).with(current_players: 2)
         post worlds_join_world_path, params: { id: 'world_1' }
       end
 
       it 'can\'t join a full world' do
-        allow(world).to receive(:current_players).and_return(2)
-        allow(world).to receive(:max_player).and_return(2)
+        allow(world).to receive_messages(current_players: 2, max_player: 2)
         post worlds_join_world_path, params: { id: 'world_1' }
         expect(response).to redirect_to worlds_path
       end
