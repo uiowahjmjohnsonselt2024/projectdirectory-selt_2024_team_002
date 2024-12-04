2bbaa0b1740ebcc59bd50c47cff206d06ca4e089
adervesh03 <adervesh03@gmail.com>
2024-12-04 14:13:21 -0600

    add mailing layouts

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@
 
 # WIP class for forget password emails
 class UserMailer < ApplicationMailer
+  layout false
   def send_reset_password_email(user)
     @reset_url = reset_password_url(token: user.reset_password_token)
     Rails.logger.debug "Generated Reset URL: #{@reset_url}"
