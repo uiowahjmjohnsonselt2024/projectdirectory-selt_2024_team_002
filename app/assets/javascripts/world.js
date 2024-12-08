caa3a09a4db119f346ef3e7b6e6c7817c11f415d
Alex Guo <aguo2@uiowa.edu>
2024-12-08 14:32:38 -0600

    wip

--- app/assets/javascripts/world.js
+++ app/assets/javascripts/world.js
@@ -1,1 +1,23 @@
-alert('js loaded')
+$(function () {
+    // it needs to be in here, or j query will not find shit!
+    $(".grid_cell").each((index, cell) => {  // Note: the second argument is the actual DOM element
+        $(cell).click(() => {
+            const cellID = $(cell).find(".hidden").text()
+            const rowCol = cellID.split('-')
+            const row = rowCol[0]
+            const col = rowCol[1]
+            $(".modal").css("display", "flex")
+            $(".modal").find('h2').html(`Are you sure you want to move to ${row}, ${col}?`)
+        });
+        
+
+    });
+
+    $(".x").click(() => {
+        $(".modal").css("display", "none")
+
+
+    })
+
+
+});
