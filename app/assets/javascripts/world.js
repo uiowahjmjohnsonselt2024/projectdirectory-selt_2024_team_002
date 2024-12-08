bb53ca467ee988a40588fbc3c2244336351f4f74
Alex Guo <aguo2@uiowa.edu>
2024-12-08 15:39:44 -0600

    done

--- app/assets/javascripts/world.js
+++ app/assets/javascripts/world.js
@@ -1,6 +1,9 @@
+function isFreeMove(player_row, player_col, dest_row, dest_col) {
+    const rowDiff = Math.abs(player_row - dest_row);
+    const colDiff = Math.abs(player_col - dest_col);
+    return (rowDiff === 1 && colDiff === 0) || (rowDiff === 0 && colDiff === 1);
+}
 $(function () {
-    
-
     // it needs to be in here, or j query will not find shit!
     $(".grid_cell").each((index, cell) => {  // Note: the second argument is the actual DOM element
         $(cell).click(() => {
@@ -11,11 +14,15 @@ $(function () {
             if (parseInt(row) === usr_row && parseInt(col) === usr_col) {
                 $(".modal").css("display", "flex")
                 $(".modal").find('h2').html(`You already are at ${row}, ${col}!`)
+                $(".modal").find('h3').html("")
                 $(".modal").find('button').css('display', 'none')
             }
             else {
                 $(".modal").css("display", "flex")
-                $(".modal").find('h2').html(`Are you sure you want to move to ${row}, ${col}?`)
+                const isFreeText = isFreeMove(parseInt(usr_row),parseInt(usr_col), row, col) ? 'This move is free!' : 'This move costs 0.75 shards!'
+                $(".modal").find('h2').html(`Are you sure you want to move to ${row}, ${col}? `)
+                $(".modal").find('h3').html(`${isFreeText}`)
+
                 $(".modal").find('button').css('display', 'flex')
             }
         });
