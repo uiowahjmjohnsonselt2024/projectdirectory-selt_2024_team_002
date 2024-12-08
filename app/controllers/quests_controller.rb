1546a3c29203cf7d029a18e5d8fd7c391ca9be67
NikHerdt <herdtnikhil@gmail.com>
2024-12-08 13:36:42 -0600

    Added tests for quests

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,17 +1,21 @@
 class QuestsController < ApplicationController
-    def complete
-        quest = Quest.find(params[:id])
-        quest.complete
-        redirect_to world_path(quest.world)
-    end
+  def complete
+    quest = Quest.find(params[:id])
+    quest.complete
+    redirect_to world_path(quest.world)
+  end
 
-    def complete_trivia
-        quest = Quest.find(params[:id])
-        if quest.complete_trivia(params[:answer])
-          flash[:notice] = "Correct answer! Quest completed."
-        else
-          flash[:alert] = "Incorrect answer. Try again."
-        end
-        redirect_to world_path(quest.world)
-      end 
+  def complete_trivia
+    quest = Quest.find(params[:id])
+    if quest.complete_trivia(params[:answer])
+      flash[:notice] = "Correct answer! Quest completed."
+    else
+      flash[:alert] = "Incorrect answer. Try again."
+    end
+    redirect_to world_path(quest.world)
+  end
+
+  def show
+    @quest = Quest.find(params[:id])
+  end
 end
--- spec/requests/worlds_controller_spec.rb
+++ spec/requests/worlds_controller_spec.rb
@@ -69,28 +69,26 @@ describe WorldsController do
 
     # rubocop:disable RSpec/VerifiedDoubles
     it 'renders the show template on show call' do
-      world = double('world')
+      world = instance_double('World')
       collection = double('col')
       gridsquare = double('gs')
       image = double('image')
+      allow(World).to receive(:dim).and_return(1) # Mock the class method
       allow(world).to receive_messages(
-        dim: 1,
         init_if_not_inited: world,
         gridsquares: collection,
         id: 0,
-        "[]": 0
+        "[]": 0,
+        generate_quest_for: nil
       )
-      allow(World).to receive_messages(
-        find: world,
-        dim: 1
-      )
+      allow(collection).to receive(:to_ary).and_return([gridsquare])
       allow(gridsquare).to receive_messages(
         row: 1,
         col: 1,
         image: image
       )
       allow(image).to receive(:attached?).and_return(false)
-      allow(collection).to receive(:to_ary).and_return([gridsquare])
+      allow(World).to receive(:find).and_return(world)
       get '/worlds/1'
       expect(response).to render_template('show')
     end
