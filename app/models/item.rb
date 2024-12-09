e698941e89ec90db8595dec02ff97e56bc9f8eb8
adervesh03 <adervesh03@gmail.com>
2024-12-09 16:09:19 -0600

    clarify relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@
 
 # Class that represents an item
 class Item < ApplicationRecord
+  has_many :shop_items, dependent: :destroy
+  has_many :shops, through: :shop_items
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,4 +2,6 @@
 
 # Class that represents a shop
 class Shop < ApplicationRecord
+  has_many :shop_items, dependent: :destroy
+  has_many :items, through: :shop_items
 end
