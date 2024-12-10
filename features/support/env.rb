b4af9033d448fd771ef622be9364278e5c769417
Alex Guo <aguo2@uiowa.edu>
2024-12-09 18:43:01 -0600

    trying to fix

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,8 +6,20 @@ require 'rspec/expectations'
 Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 
 Capybara.default_selector = :css
-Capybara.default_driver = :selenium_chrome # Use Selenium with Chrome
-Capybara.javascript_driver = :selenium_chrome # Use Selenium with Chrome for JS tests
+
+# Dynamically configure Selenium with a unique user-data-dir for every test session
+Capybara.register_driver(:selenium_chrome) do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  # Create a unique directory for every Chrome session to prevent conflicts
+  options.add_argument("--user-data-dir=#{Dir.mktmpdir}")
+  options.add_argument('--headless') # Use headless mode if you want tests without a visible browser
+  options.add_argument('--disable-gpu')
+  options.add_argument('--window-size=1920,1080')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :selenium_chrome
 
 ActionController::Base.allow_rescue = false
 
