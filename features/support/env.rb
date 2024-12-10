682ba6eb1430624628f22b3ec93a7f24f4458cde
Alex Guo <aguo2@uiowa.edu>
2024-12-09 21:36:18 -0600

    hacking userdata

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,21 @@ require 'rspec/expectations'
 
 Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 
+Capybara.register_driver :selenium_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  # Pass custom arguments here
+  options.add_argument('--headless') # Run in headless mode
+  options.add_argument('--disable-gpu') # Disable GPU
+  options.add_argument('--window-size=1920,1080') # Set a default window size
+  options.add_argument('--no-sandbox') # Avoid sandboxing issues
+  options.add_argument('--disable-dev-shm-usage') # Fix some environments like Docker
+  unless ENV['CHROME_USER_DATA_DIR'].nil?
+    options.add_argument("--user-data-dir=#{ENV['CHROME_USER_DATA_DIR']}") # Fix some environments like Docker
+  end
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
 Capybara.default_selector = :css
 Capybara.default_driver = :selenium_headless # Use Selenium with Chrome
 Capybara.javascript_driver = :selenium_chrome # Use Selenium with Chrome for JS tests
