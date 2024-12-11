f7c38d4089edb5f048f0ffd394b41399c9cfc9d8
Alex Guo <aguo2@uiowa.edu>
2024-12-11 11:17:54 -0600

    done listing tests

--- spec/requests/quest_controller_spec.rb
+++ spec/requests/quest_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'quests', type: :request do
   describe 'not logged in' do
     it 'redirects correctly if the user is not logged in' do
       allow(User).to receive(:find_user_by_session_token).and_return(nil)
-      post cell_quest_path
+      post generate_quests_path	
       # expect(response).to redirect_to users_login_path
     end
   end
@@ -18,6 +18,30 @@ RSpec.describe 'quests', type: :request do
       usr = instance_double(User)
       allow(User).to receive(:find_user_by_session_token).and_return(usr)
       allow(usr).to receive(:id).and_return(1)
+    end
+    
+    it 'calls the complete quest method for the complete route' do
+      
+    end
+
+    it 'redirects to the world path for the complete route' do
+      
+    end
+
+    it 'calls the complete trivia model method for complete trivia route' do 
+      
+    end
+
+    it 'redirects to the world path for complete trivia route' do 
+      
+    end
+  
+    it 'finds the quest for the show route' do
+      
+    end
+
+    it 'calls the generate quest for the generate route' do
+      
     end
   end
 end
