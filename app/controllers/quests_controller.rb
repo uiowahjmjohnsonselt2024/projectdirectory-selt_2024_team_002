7cf651c9743f44293eaae753f431c0d0b5a569ba
NikHerdt <herdtnikhil@gmail.com>
2024-12-13 19:22:53 -0600

    fixed linting and tests

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -25,11 +25,11 @@ class QuestsController < ApplicationController
 
   def complete_trivia
     quest = Quest.find(params[:id])
-    if quest.complete_trivia(params[:answer])
-      flash[:alert] = 'Correct answer! Quest completed.'
-    else
-      flash[:alert] = 'Incorrect answer.'
-    end
+    flash[:alert] = if quest.complete_trivia(params[:answer])
+                      'Correct answer! Quest completed.'
+                    else
+                      'Incorrect answer.'
+                    end
     redirect_to world_path(quest.world)
   end
 
