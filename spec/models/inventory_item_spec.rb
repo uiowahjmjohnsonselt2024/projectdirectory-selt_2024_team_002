551bafd78bb64ead11141e55ad831806a6ed4f70
adervesh03 <adervesh03@gmail.com>
2024-12-15 00:47:16 -0600

    inventory items and user world item unit tests

--- spec/models/inventory_item_spec.rb
+++ spec/models/inventory_item_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe InventoryItem, type: :model do
     allow(inventory).to receive(:item_id).and_return("2")
     expect(inventory.item_id).to eq("2")
   end
+
+  it 'should boost XP' do
+    inventory_item = instance_double(described_class)
+    allow(inventory_item).to receive(:consume_item).and_return("XP Boost")
+    expect(inventory_item.consume_item).to eq("XP Boost")
+  end
+
+  it 'should increase speed' do
+    inventory_item = instance_double(described_class)
+    allow(inventory_item).to receive(:consume_item).and_return("Speed Potion")
+    expect(inventory_item.consume_item).to eq("Speed Potion")
+  end
+
+  it 'should increase luck' do
+    inventory_item = instance_double(described_class)
+    allow(inventory_item).to receive(:consume_item).and_return("4 Leaf Clover")
+    expect(inventory_item.consume_item).to eq("4 Leaf Clover")
+  end
 end
