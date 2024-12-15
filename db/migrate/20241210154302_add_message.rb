cd5d30e58bd5b2acd24fb6a293ebafd719f91fe3
Alex Guo <aguo2@uiowa.edu>
2024-12-15 01:52:35 -0600

    added fk strat

--- db/migrate/20241210154302_add_message.rb
+++ db/migrate/20241210154302_add_message.rb
@@ -3,8 +3,8 @@
 class AddMessage < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
-      t.references :world, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.references :world, null: false, foreign_key: true, dependent: :destroy
+      t.references :user, null: false, foreign_key: true, dependent: :destroy
       t.text :message, null: false
       t.timestamps
     end
