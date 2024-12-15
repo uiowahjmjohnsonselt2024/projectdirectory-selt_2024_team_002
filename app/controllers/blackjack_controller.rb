829fa9e64194be9f0ceb2c60eab66bec5bc2d4c3
adervesh03 <adervesh03@gmail.com>
2024-12-14 19:05:14 -0600

    update user credits upon buy in and then update only if they win or push

--- app/controllers/blackjack_controller.rb
+++ app/controllers/blackjack_controller.rb
@@ -13,32 +13,32 @@ class BlackjackController < ApplicationController
     redirect_to users_login_path
   end
 
-  # rubocop:disable Metrics/MethodLength
   def update_user_credits
     @cur_user = User.find_user_by_session_token(cookies[:session])
-    unless @cur_user
-      redirect_to users_login_path, alert: 'Please login' and return
-    end
+    redirect_to users_login_path, alert: 'Please login' and return unless @cur_user
 
     @world = World.find(params[:world_id])
     @user_world = UserWorld.find_by(user: @cur_user, world: @world)
 
-    unless @user_world
-      redirect_to world_path(@world), alert: 'User does not belong to this world' and return
-    end
+    redirect_to world_path(@world), alert: 'User does not belong to this world' and return unless @user_world
 
     @gridsquare = Gridsquare.find_by_row_col(@world.id, @user_world.user_row, @user_world.user_col)
 
-    case params[:result]
-    when 'win'
-      @cur_user.charge_credits(-@gridsquare.buy_in_amount)
-    when 'loss'
-      @cur_user.charge_credits(@gridsquare.buy_in_amount)
-    else
-      redirect_to world_path(@world), alert: 'Invalid result' and return
-    end
+    process_result(params[:result])
 
     render json: { shard_balance: @cur_user.available_credits }, status: :ok
   end
-  # rubocop:enable Metrics/MethodLength
+
+  def process_result(result)
+    case result
+    when 'win'
+      @cur_user.charge_credits(-2 * @gridsquare.buy_in_amount)
+    when 'buy_in'
+      @cur_user.charge_credits(@gridsquare.buy_in_amount)
+    when 'push'
+      @cur_user.charge_credits(-@gridsquare.buy_in_amount)
+    else
+      redirect_to world_path(@world), alert: 'Invalid result' and return
+    end
+  end
 end
